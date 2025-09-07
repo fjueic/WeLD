b657bc4522fde45dfa8a44350ba672d0513fe743
Nathan22211 <35241068+Nathan22211@users.noreply.github.com>
2025-09-07 12:20:56 -0500

    update webkit 2 version

--- weld/gi_modules/__init__.py
+++ weld/gi_modules/__init__.py
@@ -3,7 +3,7 @@ import gi
 # Declare all versions before importing
 gi.require_version("GLib", "2.0")
 gi.require_version("Gtk", "3.0")
-gi.require_version("WebKit2", "4.0")
+gi.require_version("WebKit2", "4.1")
 gi.require_version("GtkLayerShell", "0.1")
 gi.require_version("Gdk", "3.0")
 
